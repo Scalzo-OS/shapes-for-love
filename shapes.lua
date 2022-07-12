f3568b22394ed65d6ca163aec207d4e52a3caf4d
scalzo-os <92215010+Scalzo-OS@users.noreply.github.com>
2022-07-12 15:37:12 +1000

    added collision detection for bezels

--- shapes.lua
+++ shapes.lua
@@ -20,4 +20,40 @@ function shapes.bezel(x, y, w, h, r)
     love.graphics.rectangle('fill', rx-rd, ry+rh, rw+2*rd, r+rd)
 end
 
+function shapes.bezelcollision(x, y, w, h, r, mx, my)
+    local rd = math.sqrt(r^2/2)
+    local rw, rh = w-2*r-2*rd, h-2*r-2*rd
+    local rx, ry = x+r+rd, y+r+rd
+    local t = false
+
+    circles = {
+        {x = rx-rd, y = ry-rd},
+        {x = rx+rw+rd, y = ry-rd},
+        {x = rx-rd, y = ry+rh+rd},
+        {x = rx+rw+rd, y = ry+rh+rd}
+    }
+
+    rects = {
+        {x = rx-r-rd, y = ry-rd, w = 2*r, h = rh+2*rd},
+        {x = rx+rw+rd-r, y = ry-rd, w = 2*r, h = rh+2*rd},
+        {x = rx-rd, y = ry-rd-r, w = rw+2*rd, h = r+rd},
+        {x = rx-rd, y = ry+rh, w = rw+2*rd, h = r+rd}
+    }
+
+    for i=1, #circles do
+        if math.sqrt((mx-circles[i].x)^2+(my-circles[i].y)^2) < r then
+            t = true
+        end
+    end
+
+    for i=1, #rects do
+        if mx < rects[i].x + rects[i].w and mx > rects[i].x and my > rects[i].y 
+        and my < rects[i].y + rects[i].h then
+            t = true
+        end
+    end
+
+    return t
+end
+
 return shapes
